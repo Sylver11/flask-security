e38a30d1303f6854e2e4d9e91d92d5d581c932b0
jsvoigt <justus@bcity.me>
2021-04-13 17:46:55 +0200

    adding decortor to flask_security for more modularity

--- flask_security/decorators.py
+++ flask_security/decorators.py
@@ -1,5 +1,41 @@
+from flask import jsonify, current_app, has_request_context, request
+from flask_login import current_user
 from werkzeug.exceptions import Forbidden
+import sys, traceback
 import functools
+import logging
+
+
+
+def catch_view_exception(f):
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as ex:
+            template = 'User uuid:{}\nIP:{}\nRequested URL:{}\nTraceback:{}'
+            request_remote_addr = request_url = user_uuid = 'Unknown'
+            post = False
+            if has_request_context():
+                request_url = request.url
+                request_remote_addr = request.remote_addr
+                if request.method == 'POST':
+                    post = True
+                if current_user.is_authenticated:
+                    user_uuid = current_user.uuid
+            admin_error_message = template.format(
+                    user_uuid,
+                    request_remote_addr,
+                    request_url,
+                    traceback.format_exc())
+            current_app.logger.error(admin_error_message)
+            template = 'An exception of type {0} occurred. Arguments:\n{1!r}'
+            user_error_message = template.format(type(ex).__name__, ex.args)
+            if post:
+                return jsonify(user_error_message), 500
+            return user_error_message
+    return inner
+
 
 def import_user():
     try:
